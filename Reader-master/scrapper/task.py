b8a923c01a34c80f8d4e1369ee74fddf4515be70
MICHAELFOLA <67244206+MICHAELFOLA@users.noreply.github.com>
2021-08-08 17:25:30 -0400

    update

--- Reader-master/scrapper/task.py
+++ Reader-master/scrapper/task.py
@@ -3,12 +3,12 @@ from selenium import webdriver
 from django.http import JsonResponse
 from django.views.generic import ListView
 from selenium.webdriver.common.by import By
-import os
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+# import os
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--no-sandbox")
 
 # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 import chromedriver_binary  # Adds chromedriver binary to path
