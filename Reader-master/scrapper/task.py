c3de5379ad4abc77d127ea5dddc53e4432d9d062
MICHAELFOLA <67244206+MICHAELFOLA@users.noreply.github.com>
2021-08-08 16:29:58 -0400

    update

--- Reader-master/scrapper/task.py
+++ Reader-master/scrapper/task.py
@@ -9,7 +9,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 import chromedriver_binary  # Adds chromedriver binary to path
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
